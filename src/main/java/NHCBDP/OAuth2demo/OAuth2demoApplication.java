273ac8d2c99153b5c828087b9c3de91c731db015
文标 <liuwenbiao@cecdat.com>
2017-09-08 15:59:15 +0800

    @wbliu first commit

---
+++ src/main/java/NHCBDP/OAuth2demo/OAuth2demoApplication.java
@@ -0,0 +1,12 @@
+package NHCBDP.OAuth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2demoApplication.class, args);
+	}
+}
