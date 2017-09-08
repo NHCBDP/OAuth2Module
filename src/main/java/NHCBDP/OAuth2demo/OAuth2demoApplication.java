f580274e8eb77ace54529f2c0d2ead4b15b0c9d3
文标 <liuwenbiao@cecdat.com>
2017-09-08 18:10:55 +0800

    @wbliu simple sample for  oauthorization code grant demo

--- src/main/java/NHCBDP/OAuth2demo/OAuth2demoApplication.java
+++ src/main/java/NHCBDP/OAuth2demo/OAuth2demoApplication.java
@@ -2,8 +2,10 @@ package NHCBDP.OAuth2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class OAuth2demoApplication {
 
 	public static void main(String[] args) {
